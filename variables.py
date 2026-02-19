9cdaec0435745a7ecb619ccee60ecabe38834733
namrathav09 <namrathavenkateshc@gmail.com>
2026-02-19 11:42:25 +0530

    Added Variables

---
+++ variables.py
@@ -0,0 +1,30 @@
+#allowed
+myage=22
+my_age=22
+_my_age=22
+myAge=22
+MYAGE=22
+myage1=22
+
+#wrong/illegal
+#1myage=22
+#my var=22
+#my-age=22
+
+a,b,c="kavya","ramya","sowmya"
+print(a)
+print(b)
+print(c)
+d=e=f="kavya"
+print(d)
+print(e)
+print(f)
+
+name="Namratha"
+age=22
+height=5.4
+is_married=True
+
+print("Hi I am ",name)
+print("I am ",age,"years old")
+print("My height",height)
