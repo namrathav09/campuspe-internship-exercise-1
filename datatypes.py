839ed96cdebe0166c43f3f57250c50066b49c579
namrathav09 <namrathavenkateshc@gmail.com>
2026-02-19 19:49:32 +0530

    Added Datatypes related Files and KT items

---
+++ datatypes.py
@@ -0,0 +1,46 @@
+#strings
+#message="Hey bro wassup"
+#name='python'
+
+#poem="""
+#Roses are red ,
+#Violets are blue,
+#Python is awesome,
+#Well so are you
+#"""
+#string operators
+#first_name="Namratha"
+#last_name="Venkatesh"
+#concatenate
+#fullname=first_name+" "+last_name
+#print(fullname)
+#repeatative characters
+#line  = "*"*len(fullname)
+#print(line)
+#print(fullname[6])
+#Integer
+#a=10
+#b=20
+
+#print("addition",a+b))
+#print("subtraction",a-b)
+#print("multiplication",a*b)
+#print("division",a/b)
+#print("integer division",a//b)
+#print("modulus",a%b)
+#print("exponential",a**b)
+#float
+price=20.99
+PI=3.14285
+temp=99.8
+total=price*1.5
+print(total)
+
+print(0.1+0.345)
+print(round(0.1+0.345,5))
+#boolean
+is_raining=False
+is_holiday=False
+age=10
+is_adult=age>=18
+
