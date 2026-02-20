391983410ab088ef979e3624d0cd28287fa52001
namrathav09 <namrathavenkateshc@gmail.com>
2026-02-20 19:02:12 +0530

    Added conditional statements

---
+++ conditional-statements.py
@@ -0,0 +1,77 @@
+name_str=input("What is your name?")
+age= input ("Hi "+name_str+", What is your age? " )
+
+password="123123123"
+
+#IF ELSE CONDITION
+# if password=="secret@123":
+#     print("Access granted")
+# else:
+#     print("Access denied")
+
+
+# if int(age)>=18:
+#     print("You are a Adult")
+#     print("You can vote!")
+# else:
+#     print("You are bacha")
+#     print("Go have some ice cream")
+
+#IF ELSE IF
+# score=85
+# if score>=90:
+#     grade="A"
+# elif score>=80:
+#     grade="B"
+# elif score>=70:
+#     grade="C"
+# elif score>=60:
+#     grade="D"
+# else:
+#     grade="F"
+
+# print("Your grade is:",grade)
+
+# age=25
+# has_license=False
+
+# if age>18:
+#     print("You are old enough to drive")
+
+#     if has_license:
+#         print("You can drive")
+#     else:
+#         print("But you need a license")
+# else:
+#     print("Too young to drive")
+
+#If else using "AND" condition
+# age=20
+# has_ticket=True
+# if age>=18 and has_ticket:
+#     print("You can enter the concert")
+# else:
+#     print("Cannot enter")
+
+#IF else using"OR"
+# day="saturday"
+# if day =="saturday"or day=="sunday":
+#     print("It is the weekend open the book")
+# else:
+#     print("It is weekday")
+
+# is_sunny=True
+
+# if not is_sunny:
+#     print("Its cloudy")
+# else:
+#     print("Its sunny")
+
+age=21
+# if age>=18:
+#     status="adult"
+# else:
+#     status="mine"
+
+status = "adult" if age>=19 else "minor"
+print(status)
