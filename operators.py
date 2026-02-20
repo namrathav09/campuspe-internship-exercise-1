a99e44b12af8eb95c6c48fd9334adcc8cd3b3c07
namrathav09 <namrathavenkateshc@gmail.com>
2026-02-20 07:27:37 +0530

    Added Operators and User-input.

---
+++ operators.py
@@ -0,0 +1,34 @@
+#Arithmatic Operators
+#+ print(a+b)   -> Addition
+#- print(a-b)   -> Subtraction
+#* print(a*b)   -> Multiplication
+#/ print(a/b)   -> Division
+#// print(a//b) -> Floor Division
+#% print(a%b)   -> Modulus
+#** print(a**b) -> Epxonentiation
+
+#Comparison Operators
+# == -> Equal to                        -> print(x==y)
+# != -> Not Equal to                    -> print(x!=y)
+# >  -> Greater than                    -> print(x>y)
+# <  -> Lesser than                     -> print(x<y)
+# >= -> Greater than or Equal to        -> print(x>=y)
+# <= -> Lesser than or Equal to         -> print(x<=y)
+
+#Logical Operators
+#and    ->  AND Operator                
+#or     ->  OR operator
+#not    ->  NOT Operator
+
+#Assignment Operators
+# a=120
+# a+=1
+# a-=1
+# a*=1
+# a/=1
+
+#PEMDAS
+#(Parenthesis, Exponents, Multiply/Divide, Add/Subtract)
+
+result = (2 + 3) * 4
+print(result)
