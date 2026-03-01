06b647876623c70e14ef3790c47879ee8e20134d
namrathav09 <namrathavenkateshc@gmail.com>
2026-03-01 21:56:24 +0530

    Added file

--- file.py
+++ file.py
@@ -1,3 +1,15 @@
-file=open("test.text","r")
-content=file.read()
-print(content)
+# file=open("test.txt","r")
+# content=file.read()
+# print(content)
+# file.close()
+# with open("test.txt","r") as file:
+#     lines=file.readline()
+#     for line in lines:
+#         print(line.strip())
+
+# with open("output.txt","w")as file:
+#     file.write("Hello world\n")
+#     file.write("This is a new file\n")
+with open("output.txt","a")as file:
+    file.write("Hello world\n")
+    file.write("This is a new file\n")
