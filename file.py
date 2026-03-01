139a6a9c075524c467426a2004bb044315e7e997
namrathav09 <namrathavenkateshc@gmail.com>
2026-03-01 21:31:33 +0530

    Added modules

---
+++ file.py
@@ -0,0 +1,3 @@
+file=open("test.text","r")
+content=file.read()
+print(content)
---
+++ modules.py
@@ -0,0 +1,12 @@
+# from math  import pi, sqrt
+# print(pi)
+# print(sqrt(9))
+
+# import math
+# print(math.pi)
+
+import datetime
+import os
+
+print(datetime.datetime.now())
+print(os.getcwd())
